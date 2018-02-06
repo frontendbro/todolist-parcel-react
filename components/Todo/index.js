24494ef4085599640863b6e7e93518592ffad74d
szadorozhniy <szadorozhniy@axiomsl.com>
2018-02-06 20:01:09 +0300

    new eslintrc

--- components/Todo/index.js
+++ components/Todo/index.js
@@ -3,16 +3,15 @@ import './index.css';
 import Checkbox from '../Checkbox/index';
 
 function Todo({ dataTodo }) {
-  return (
-    <div className='todo'>
-      <Checkbox checked={dataTodo.completed} />
-      <div className='todo-title'> {dataTodo.title} </div>
-      <button className='delete icon'>
-        <i className='material-icons'>delete</i>
-      </button>
-    </div>
-  );
+	return (
+		<div className='todo'>
+			<Checkbox checked = { dataTodo.completed } />
+			<div className = 'todo-title'> { dataTodo.title } </div>
+			<button className='delete icon'>
+				<i className='material-icons'>delete</i>
+			</button>
+		</div>
+	);
 }
-
 
 export default Todo;
--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import logo from "./logo.svg";
-import "./index.css";
-import data from "./data";
-import Todo from "./components/Todo";
-import Header from "./components/Header";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './index.css';
+import data from './data';
+import Todo from './components/Todo';
+import Header from './components/Header';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <img className="App-Logo" src={logo} alt="React Logo" />
-        <div className="wrapper">
-          <Header />
-          <section className="todo-list">
-            {this.props.data.map(todo => (
-              <Todo dataTodo={todo} key={todo.id}/>
-            ))}
-          </section>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<img className='App-Logo' src={logo} alt='React Logo' />
+				<div className='wrapper'>
+					<Header />
+					<section className='todo-list'>
+						{this.props.data.map(todo => (
+							<Todo dataTodo={todo} key={todo.id}/>
+						))}
+					</section>
+				</div>
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(<App data={data}/>, document.getElementById("root"));
+ReactDOM.render(<App data={data}/>, document.getElementById('root'));
